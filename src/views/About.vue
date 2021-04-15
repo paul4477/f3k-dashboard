1017f1e23c14f6a82f133453f57477ceffa77d71
Paul Robinson <paul.d.robinson@gmail.com>
2021-04-15 09:28:46 +0100

    Updated about info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="jumbotron">
-  <h1 class="display-4">f3k_vue </h1>
-  <p class="lead">f3k_vue is a web-based app for self timing and recording results on field during F3K competitions.<br />
-  This app integrates with GliderScore by Gerry Carter for competition setup and management.</p>
+  <h1 class="display-4">F3K Dashboard</h1>
+  <p class="lead">F3K Dashboard is a web-based app for providing round information at the field during F3K competitions.<br />
+  This app integrates with F3Vault.com by Tim Traver for competition setup and management and GliderScore by Gerry Carter for live running via the GliderScore "Digital Timer".</p>
   <hr class="my-4">
   <p>Developed by <a href="https://github.com/paul4477">Paul Robinson</a>.</p>
   </div>
