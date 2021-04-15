869049c96412c72996f879510b7a0f2608094e40
Paul Robinson <paul.d.robinson@gmail.com>
2021-04-15 09:02:01 +0100

    Basic structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App deployed to heroku"/>
+<div>
+  <div class="row">
+    <div class="col-lg">
+      Hello
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
+  name: 'Home',
   components: {
-    HelloWorld
   }
-};
+}
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '^/scoringdata.*': {
+        target: 'http://192.168.64.184',
+        ws: true,
+        changeOrigin: true,
+        logLevel: 'debug'
+      }
+    }
+  }
 };
