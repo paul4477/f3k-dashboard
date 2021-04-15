ef14032459be3a887ee83bff4dc21eaaafc5fad1
Paul Robinson <paul.d.robinson@gmail.com>
2021-04-15 08:49:01 +0100

    None

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
+Vue.$cookies.config('12h')
+
 Vue.config.productionTip = false;
 
 new Vue({
