174c3223902c691fcf2da8ec5d2353cba418713c
Paul Robinson <paul.d.robinson@gmail.com>
2021-04-15 09:23:58 +0100

    Set wbepack app name

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   lintOnSave: false,
-  devServer: {
-    proxy: {
-      '^/scoringdata.*': {
-        target: 'http://192.168.64.184',
-        ws: true,
-        changeOrigin: true,
-        logLevel: 'debug'
-      }
-    }
-  }
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+      args[0].title = 'F3K Dashboard';
+      return args;
+    });
+  }  
 };
