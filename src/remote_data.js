dbe79cc786da429b60cc6f6f1498dd813872c8a7
Paul Robinson <paul.d.robinson@gmail.com>
2021-04-21 22:03:31 +0100

    Correct message

--- src/remote_data.js
+++ src/remote_data.js
@@ -4,8 +4,8 @@ NProgress.configure({showSpinner: true})
 //NProgress.configure({parent: '#appmain'})
 //NProgress.configure({trickleSpeed: 200, trickleRate: 0.1,  trickle: true, easing: 'ease', speed: 50});
 
-const APIPASS = process.env.VUE_APP_APIPASS || process.env.APIPASS || 'set.ENV.VUE_APP_APIPASS';
-const APIUSER = process.env.VUE_APP_APIUSER || process.env.APIUSER || 'set.ENV.VUE_APP_APIUSER';
+const APIPASS = process.env.VUE_APP_APIPASS || process.env.APIPASS || 'set ENV.VUE_APP_APIPASS';
+const APIUSER = process.env.VUE_APP_APIUSER || process.env.APIUSER || 'set ENV.VUE_APP_APIUSER';
 
 // create a new axios instance
 const instance = axios.create({
