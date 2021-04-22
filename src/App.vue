1420d844a806651431065545346bde665079562e
Paul Robinson <paul.d.robinson@gmail.com>
2021-04-22 10:30:34 +0100

    Added F3XVaault links

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
   <div id="app">
       <nav class="navbar navbar-dark bg-dark">
+        
         <router-link class="navbar-brand" class-active="nav-link active" to="/">
         <img src="./assets/glider15.svg" height="25" alt="">
         </router-link>
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          </nav>
-        <div class="navbar navbar-dark bg-dark collapse navbar-collapse" id="collapsibleNavbar">
-                        
-                        <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-primary" class-active="btn btn-outline-primary" to="/event">Groups</router-link>
-                        <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-primary" class-active="btn btn-outline-primary" to="/roundstatus">Live</router-link>
-                        <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-primary" class-active="btn btn-outline-primary" to="/scoring">Scoring</router-link>
-                        <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-primary" class-active="btn btn-outline-primary" to="/scoring">Results</router-link>
-                        <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-primary" class-active="btn btn-outline-primary" to="/about">About</router-link>
+        </button>
+      </nav>
+      
+      <div class="navbar navbar-dark bg-dark collapse navbar-collapse" id="collapsibleNavbar">
+        <div class="btn-group" role="toolbar">
+          <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-primary" class-active="btn btn-outline-primary" to="/event">Groups</router-link>
+          <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-primary" class-active="btn btn-outline-primary" to="/roundstatus">Live</router-link>
+          <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn btn-outline-primary" class-active="btn btn-outline-primary" to="/scoring">Scoring</router-link>
+          
+          <div v-if="$store.state.currentComp.populated" class="btn-group" role="group">
+          <form name="f3xvault_self_entry" method="POST" v-bind:action="'https://www.f3xvault.com/?action=event&function=event_view&event_id=' + $store.state.currentComp.eventDataRaw.event_id">
+            <input type="hidden" name="action" value="event">
+            <input type="hidden" name="function" value="event_self_entry">
+            <input type="hidden" name="event_id" v-bind:value="$store.state.currentComp.eventDataRaw.event_id">
+          </form>
+            <button id="f3xbuttongrp" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              F3XVault
+            </button>
+            
+            <div class="dropdown-menu" aria-labelledby="f3xbuttongrp">
+              <a class="dropdown-item" v-bind:href="'https://www.f3xvault.com/?action=event&function=event_view&event_id=' + $store.state.currentComp.eventDataRaw.event_id">View Event</a>
+              <a class="dropdown-item" onclick="document.f3xvault_self_entry.submit()" href="#">Scoring</a>
+            </div>
+          </div>
+          
+        </div>
+      
+        
+                        <!--<div class="btn-group" role="group">
+                        <router-link data-toggle="collapse" data-target="#collapsibleNavbar" tag="button" class="btn-sm btn-primary" class-active="btn btn-outline-primary" to="/about">About</router-link>
+                        </div>     --> 
         <!--<ul class="navbar-nav">
         <li class="nav-item"><router-link data-toggle="collapse" data-target="#collapsibleNavbar" class="nav-link" class-active="nav-link active" to="/roundstatus">Current Round</router-link></li>
         <li class="nav-item"><router-link data-toggle="collapse" data-target="#collapsibleNavbar" class="nav-link" class-active="nav-link active" to="/about">About</router-link></li>
         </ul>-->
-        </div>
+      
+      </div>
 
       
       <router-view />
@@ -28,10 +52,9 @@
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style>
